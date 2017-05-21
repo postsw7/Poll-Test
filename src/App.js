edb7a6e1e34f4d174d1e04d824210a7bd3da1fbe
Siwoo <postsw7@gmail.com>
2017-05-21 20:08:43 +0900

    add PollList component & create poll modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import PollList from './components/PollListComponent';
+import { Modal, Button, FormGroup, InputGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import * as firebase from 'firebase';
 
 class App extends Component {
@@ -73,11 +75,98 @@ class App extends Component {
     return firebase.database().ref().update(updates);
   }
 
+  open () {
+    this.setState({
+      showModal: true
+    })
+  }
+
+  close () {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  handleOptions () {
+    const pollOptions = {}
+    pollOptions[this.opOne.value] = 0;
+    pollOptions[this.opTwo.value] = 0;
+    pollOptions[this.opThree.value] = 0;
+
+    this.createNewPoll(0, this.state.username, this.state.title, pollOptions);
+    this.close();
+  }
+
+  deletePoll () {
+
+    // var newPollKey = firebase.database().ref().child('poll').push().key;
+
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/poll/'] = null;
+    updates['/user/' + 0 + '/'] = null;
+
+    return firebase.database().ref().update(updates);
+  }
+
   render() {
+    const pollModal = (
+      <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+        <Modal.Header>
+          <Modal.Title>Create Poll</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <form className="option-group" >
+          <FormGroup controlId="poll_title">
+            <ControlLabel>Set your poll title</ControlLabel>
+            <FormControl onChange={(e) => { this.setState({ title: e.target.value }); }} type="text" placeholder="type your poll title"/>
+          </FormGroup>
+          <FormGroup controlId="username">
+            <ControlLabel>Name</ControlLabel>
+            <FormControl onChange={(e) => { this.setState({ username: e.target.value }); }} type="text" placeholder="type your name"/>
+          </FormGroup>
+          <FormGroup controlId="option_1">
+            <InputGroup>
+              <InputGroup.Addon>
+                <input type="radio" aria-label="..." />
+              </InputGroup.Addon>
+              <FormControl inputRef={ref => { this.opOne = ref; }} type="text" placeholder="type your option" />
+            </InputGroup>
+          </FormGroup>
+          <FormGroup controlId="option_2">
+            <InputGroup>
+              <InputGroup.Addon>
+                <input type="radio" aria-label="..." />
+              </InputGroup.Addon>
+              <FormControl inputRef={ref => { this.opTwo = ref; }} type="text" placeholder="type your option" />
+            </InputGroup>
+          </FormGroup>
+          <FormGroup controlId="option_3">
+            <InputGroup>
+              <InputGroup.Addon>
+                <input type="radio" aria-label="..." />
+              </InputGroup.Addon>
+              <FormControl inputRef={ref => { this.opThree = ref; }} type="text" placeholder="type your option" />
+            </InputGroup>
+          </FormGroup>
+          </form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button onClick={this.close.bind(this)}>Close</Button>
+          <Button bsStyle="primary" onClick={this.handleOptions.bind(this)}>Create Poll</Button>
+        </Modal.Footer>
+
+      </Modal>
+    );
+
     return (
       <div className="App">
-        <Button bsStyle="primary">Create Poll</Button>
-        <Button>Delete Poll</Button>
+        <PollList />
+        <Button bsStyle="primary" onClick={this.open.bind(this)}>Create Poll</Button>
+        <Button onClick={this.deletePoll.bind(this)}>Delete Poll</Button>
+        {pollModal}
       </div>
     );
   }
