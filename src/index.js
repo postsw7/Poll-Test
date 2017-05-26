0c738c0f086d7d4f6f3aa5f887ce075261b80881
Siwoo <postsw7@gmail.com>
2017-05-26 19:14:29 +0900

    delete unused files & change title

--- src/index.js
+++ src/index.js
@@ -9,17 +9,6 @@ import reducers from './reducers';
 import { Provider } from 'react-redux';
 
 const store = createStore(reducers, applyMiddleware(logger));
-// import * as firebase from 'firebase';
-
-// var config = {
-//   apiKey: "AIzaSyAjy12DzoLruJbJ9XsvD8L-I-zeX81D5yc",
-//   authDomain: "classting-5d243.firebaseapp.com",
-//   databaseURL: "https://classting-5d243.firebaseio.com",
-//   projectId: "classting-5d243",
-//   storageBucket: "classting-5d243.appspot.com",
-//   messagingSenderId: "763590798520"
-// };
-// firebase.initializeApp(config);
 
 ReactDOM.render(
   <Provider store={store}>
